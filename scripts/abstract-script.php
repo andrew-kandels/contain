b32b7380e35a1207b850f0e881a46fec0bdf207a
Mike Willbanks <mike@digitalstruct.com>
2013-06-03 14:53:22 -0700

    Fixed basic installation path

--- scripts/abstract-script.php
+++ scripts/abstract-script.php
@@ -39,7 +39,7 @@ define('COMPOSER_AUTOLOADER', ZF2_APPLICATION_PATH . '/vendor/autoload.php');
 
 define('IS_PRODUCTION', false);
 define('IS_DEVELOPMENT', true);
-define('APPLICATION_CONFIG_FILE', ZF2_APPLICATION_PATH . '/config/production-application.config.php');
+define('APPLICATION_CONFIG_FILE', ZF2_APPLICATION_PATH . '/config/application.config.php');
 
 // DO NOT EDIT Below this line
 /********************************************************************************/
